43227c3f667095a1dbf76ed43b2748ad5a676a67
psimyn <psimyn@gmail.com>
2016-12-03 20:58:46 +1100

    three dumping ground

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import svelte from 'rollup-plugin-svelte'
+import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+// import livereload from 'rollup-plugin-livereload'
+
+const plugins = [
+	svelte(),
+	nodeResolve({
+    jsnext: true,
+    browser: true,
+    main: true,
+	}),
+	commonjs({
+//		namedExports: {
+//			'node_modules/three/build/three.js': ['WebGLRenderer', 'PerspectiveCamera', 'Scene', 'BoxGeometry', 'PlaneGeometry', 'MeshBasicMaterial', 'Mesh'],
+//		}
+	}),
+]
+if ( process.env.production ) plugins.push( buble(), uglify() )
+// else plugins.push(livereload())
+
+export default {
+	entry: 'src/app.js',
+	dest: 'dist/bundle.js',
+	format: 'iife',
+	plugins,
+	sourceMap: true,
+  intro: 'window.TrackballControls = window.TrackballControls || {}'
+}
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import Framer from './Framer.html';
+
+window.Framer = new Framer({
+	target: document.querySelector( '.framerapp' )
+});
